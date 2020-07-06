60e8a66629ab18764452ab9ce848822dcd8657ab
Norayr Chilingarian <norayr@arnet.am>
2020-07-06 18:18:18 +0400

    simplified example script

--- src/cron/sync_diaspora_to_matean_complicated.sh
+++ src/cron/sync_diaspora_to_matean_complicated.sh
@@ -1,11 +1,25 @@
+log="/tmp/sync_matean.log"
+echo "run" >> $log
+echo "`date`" >> $log
+mounted=`mount | grep amp`
 posts_path="/home/m/matean/content/post"
 hugo_path="/home/m/matean"
-d2h="/root/diaspora2hugo/build/d2h"
+d2h="/amp/nil/github/postgre/diaspora2hugo/src/build/d2h"
+user="norayr"
+set -x
+echo "check if mounted" >> $log
+if [[ -n "${mounted/[ ]*\n/}" ]]
+then
+  #execute if the the variable is not empty and contains non space characters
+  echo "mounted, continuing" >> $log
   cd $posts_path
   last_post=`ls $posts_path/*spyurk.md | tail -n 1 | awk -F "_" {'print $2'}`
+  echo "last post $last_post" >> $log
   tmpdir="$(mktemp -d /tmp/matean.XXXXXX)"
+  echo "tmpdir $tmpdir" >> $log
   cd $tmpdir
-  $d2h norayr $last_post
+  echo "running d2h" >> $log
+  $d2h $user $last_post 
   lst=`ls $tmpdir/`
   if [[ -n "$lst" ]]
   then
@@ -23,4 +37,11 @@ d2h="/root/diaspora2hugo/build/d2h"
   else
     echo "no new posts" >> $log
   fi
+
+
+else
+  #execute if the variable is empty or contains only spaces
+  echo "not mounted, exiting" >> $log
+fi
+
 
